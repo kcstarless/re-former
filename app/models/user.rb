10608d4c16d0e1cf92597506f82afa0803983a13
kcstarless <155853062+kcstarless@users.noreply.github.com>
2024-05-30 08:16:16 +1000

    trying confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 4, maximum: 20 }
-  validates :email, presence: true
+  validates :email, presence: true, confirmation: true
+  validates :email_confirmation, presence: true
   validates :password, presence: true
 end
